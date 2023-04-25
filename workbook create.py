ddcb8487647e056f8cac10d6cc5fa3d8165b1de9
Corey.T <115489179+Coreytsang01@users.noreply.github.com>
2023-04-26 03:11:18 +0800

    Master data xlsx create

---
+++ workbook create.py
@@ -0,0 +1,17 @@
+#%%
+import openpyxl
+import os
+
+worksheetname = ["Chek Lap Kok","Cheung Chau","Clear Water Bay","Happy Valley","HK Observatory","HK Park","Kai Tak Runway Park","Kau Sai Chau","King's Park","Kowloon City","Kwun Tong","Lau Fau Shan","Ngong Ping","Pak Tam Chung","Peng Chau","Sai Kung","Sha Tin","Sham Shui Po","Shau Kei Wan","Shek Kong","Sheung Shui","Stanley","Ta Kwu Ling","Tai Lung","Tai Mei Tuk","Tai Mo Shan","Tai Po","Tate's Cairn","The Peak","Tseung Kwan O","Tsing Yi","Tsuen Wan Ho Koon","Tsuen Wan Shing Mun Valley","Tuen Mun","Waglan Island","Wetland Park","Wong Chuk Hang","Wong Tai Sin","Yuen Long Park",
+]
+
+#%%
+workbook = openpyxl.load_workbook("Master Data.xlsx")
+
+#%%
+for worksheet in worksheetname:
+    workbook.create_sheet(worksheet)
+
+# %%
+workbook.save("Master Data.xlsx")
+# %%
