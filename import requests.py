567d807d39049b5f064d9dc98fc3179ee7c46280
Corey.T <115489179+Coreytsang01@users.noreply.github.com>
2023-04-26 05:50:01 +0800

    update the combine file date time column

--- import requests.py
+++ import requests.py
@@ -33,30 +33,69 @@ print(df_wind)
 wind_column_list = list(df_wind.columns)
 print(wind_column_list)
 
-# %%
-#   Date cleaning
-wind_data_cols = [
-    'Date',
-    'Location',
-    'Wind Direction',
-    'Wind Mean Speed',
-    'Maximum Gust'
-]
-new_df_wind = df[wind_data_cols]
+# # %%
+# #   Date cleaning
+# wind_data_cols = [
+#     'Date',
+#     'Location',
+#     'Wind Direction',
+#     'Wind Mean Speed',
+#     'Maximum Gust'
+# ]
+# #new_df_wind = df[wind_data_cols]
 
-date = df_wind['Date time']
-for i in date:
-    x = str(i)
-    x1 = [x[6:8],'/',x[4:6],'/',x[:4],' ',x[-4:-2],':',x[-2:]]
-    x1 = ''.join(x)
-    print(x1)
+# date = df_wind['Date time']
+# for i in date:
+#     x = str(i)
+#     x1 = [x[6:8],'/',x[4:6],'/',x[:4],' ',x[-4:-2],':',x[-2:]]
+#     x1 = ''.join(x)
+#     print(x1)
 # %%
+#   Combine all temporary file into one excel
 df_wind = pd.read_csv('temp file\latest_10min_wind.csv')
 df_pressure = pd.read_csv('temp file\latest_1min_pressure.csv')
 df_temperature = pd.read_csv('temp file\latest_1min_temperature.csv')
-df_humidity = pd.read_csv('temp file\latest_1min_humidity')
-concat_df = df_temperature.merge(df_wind, how = 'left', on = ['Date time', 'Automatic Weather Station'])
+df_humidity = pd.read_csv('temp file\latest_1min_humidity.csv')
+concat_df1 = df_temperature.merge(df_wind, how = 'left', on = ['Date time', 'Automatic Weather Station'])
+concat_df2 = concat_df1.merge(df_pressure, how = 'left', on = ['Date time', 'Automatic Weather Station'])
+final_combine_df = concat_df2.merge(df_humidity, how = 'left', on = ['Date time', 'Automatic Weather Station'])
+final_combine_df["Date time"] = final_combine_df["Date time"].astype(str)
+date = final_combine_df['Date time']
+for i in date:
+    x = str(i)
+    x1 = [x[6:8],'/',x[4:6],'/',x[:4],' ',x[-4:-2],':',x[-2:]]
+    x2 = ''.join(x1)
+    print(x2)
+    final_combine_df['Date time'] = final_combine_df['Date time'].replace(to_replace=i, value=x2)
+print(final_combine_df["Date time"])
+# print(final_combine_df)
+# final_combine_df.to_excel("test.xlsx", index = False )
 
-print(concat_df)
-concat_df.to_excel("test.xlsx", index = False )
+
+# %%
+# Try to do: insert data into master data.xlsx
+
+combine_excel = pd.read_excel('test.xlsx', index_col='Automatic Weather Station')
+workbook_Master = openpyxl.load_workbook('Master Data.xlsx')
+#print(combine_excel)
+
+# number of station location: 39
+Station_location = [
+"Chek Lap Kok","Cheung Chau","Clear Water Bay",
+"Happy Valley","HK Observatory","HK Park",
+"Kai Tak Runway Park","Kau Sai Chau","King's Park",
+"Kowloon City","Kwun Tong","Lau Fau Shan",
+"Ngong Ping","Pak Tam Chung","Peng Chau",
+"Sai Kung","Sha Tin","Sham Shui Po",
+"Shau Kei Wan","Shek Kong","Sheung Shui",
+"Stanley","Ta Kwu Ling","Tai Lung",
+"Tai Mei Tuk","Tai Mo Shan","Tai Po",
+"Tate's Cairn","The Peak","Tseung Kwan O",
+"Tsing Yi","Tsuen Wan Ho Koon","Tsuen Wan Shing Mun Valley",
+"Tuen Mun","Waglan Island","Wetland Park",
+"Wong Chuk Hang","Wong Tai Sin","Yuen Long Park"
+]
+workbook_Master = openpyxl.load_workbook('Master Data.xlsx')
+for i in range(38):
+    if 
 # %%
